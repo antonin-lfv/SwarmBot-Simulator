efcfadb5e4d4cd5fe8f8c7a8660d3cadb72476dc
Antonin <antoninlefevre45@icloud.com>
2023-07-23 21:52:34 +0200

    Streamlit template and config

--- src/main.py
+++ src/main.py
@@ -5,3 +5,4 @@ st.set_page_config(**Streamlit_config.page_config)
 st.markdown(Streamlit_config.CSS, unsafe_allow_html=True)
 
 st.markdown('<p class="first_titre">SwarmBot Simulator</p>', unsafe_allow_html=True)
+
